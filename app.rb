fa50c20479754fa0a3781e7afb3bd18bdf6b204e
Charles Lee <unknownsanon@gmail.com>
2023-09-21 14:37:24 +0800

    Add some login, register and logout

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'bcrypt'
+require 'sinatra/flash'
 
 require_relative 'models/book'
+require_relative 'models/review'
+require_relative 'models/user'
+
+configure do
+  enable :sessions
+  register Sinatra::Flash # configure sinatra-flash
+end
 
 get "/" do
   erb :index
@@ -36,4 +45,46 @@ post '/update-book/:id' do
   @book = Book.find(params[:id])
   @book.update(title: params[:title], author: params[:author], description: params[:description])
   erb :book
+end
+
+get '/register' do
+  erb :register
+end
+
+post '/register' do
+  @user = User.create(
+    username: params[:username],
+    password: params[:password]
+    )
+
+    if @user.save
+      redirect '/login'
+    else
+      erb :register
+    end
+  end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  @user = User.find_by(username: params[:username])
+
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/books'
+  else
+    flash[:error] = "Invalid username or password" # Set flash error message
+    erb :login
+  end
+end
+
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id])
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
